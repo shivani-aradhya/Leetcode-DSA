c35ff40af94399d35757155a69f08beeefa42661
shivani-aradhya <65466500+shivani-aradhya@users.noreply.github.com>
2021-03-08 16:50:23 +0530

    commit

---
+++ week6/stock2.py
@@ -0,0 +1,29 @@
+def stockBuySell(price, n):
+    if (n == 1):
+        return
+
+    i = 0
+    while (i < (n - 1)):
+
+        while ((i < (n - 1)) and
+               (price[i + 1] <= price[i])):
+            i += 1
+
+        if (i == n - 1):
+            break
+
+        buy = i
+        i += 1
+
+        while ((i < n) and (price[i] >= price[i - 1])):
+            i += 1
+
+        sell = i - 1
+
+        print("Buy on day: ", buy, "\t",
+              "Sell on day: ", sell)
+
+price = [100, 180, 260, 310, 40, 535, 695]
+n = len(price)
+
+stockBuySell(price, n)
